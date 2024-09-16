815275a8d03a94f1d881336f492238562306f9f0
LidoHon <lidohon10@gmail.com>
2024-09-16 09:06:47 +0300

    life cycle method

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
     const name =ref('lido');
     const status=ref('active');
     const tasks=ref(['task1','task2', 'task3', 'task4']);
+    const newTask = ref('')
     // const link='https://google.com';
     const toggleStatus=()=>{
       if(status.value==='active'){
@@ -14,7 +15,26 @@ import { ref } from 'vue';
         status.value='active';
       }
     }
-   
+    const addTask=()=>{
+      if(newTask.value.trim()!==''){
+        tasks.value.push(newTask.value);
+        newTask.value='';
+      }
+      
+    }
+    const deleteTask=(index)=>{
+      tasks.value.splice(index,1);
+    }
+   onMounted(async ()=>{
+    try {
+      const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await res.json();
+      tasks.value = data.map((task)=>task.title);
+    } catch (error) {
+      console.log(error);
+    }
+     
+   })
 
 </script>
 
@@ -24,10 +44,18 @@ import { ref } from 'vue';
   <p v-else-if="status==='pending'">user is pending</p>
   <p v-else=>user is not active</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">add task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask"/>
+    <button type="submit">add</button>
+  </form>
+
 
   <h3>tasks</h3>
   <ul>
-    <li  v-for="task in tasks" :key="task">{{ task }}
+    <li  v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
 
     </li>
   </ul>
